c36e3074a45dca9ccd0d138205e52ca68988bdc0
JUAN CARLOS PADILLA MANZO <Carlos.PadillaM@alumno.udg.mx>
2020-05-25 02:01:18 -0500

    Cambios

---
+++ El_Mundo_de_5_Peso/ObjVenta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace El_Mundo_de_5_Peso
+{
+    class ObjVenta
+    {
+        public int numVenta { set; get; }
+        public string usuario { set; get; }
+        public string articulos { set; get; }
+        public int cantidad { set; get; }
+        public decimal total { set; get; }
+        public DateTime fecha { set; get; }
+        public DateTime hora { set; get; }
+        public string estado { set; get; }
+
+        public ObjVenta() { }
+
+        public ObjVenta(int numVenta, string usuario, string articulos, int cantidad, decimal total, DateTime fecha, DateTime hora, string estado)
+        {
+            this.numVenta = numVenta;
+            this.usuario = usuario;
+            this.articulos = articulos;
+            this.cantidad = cantidad;
+            this.total = total;
+            this.fecha = fecha;
+            this.hora = hora;
+            this.estado = estado;
+        }
+    }
+}
